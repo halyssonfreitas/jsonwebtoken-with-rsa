19384dd3e6396623929034599b05df5615475844
Halysson Freitas <halyssonfreitas@gmail.com>
2023-10-17 18:03:16 -0300

    :bug: remove unnecessary await

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ import path from 'path';
 
 const privateKey = fs.readFileSync(path.resolve('./ssh-keys/id_rsa'));
 
-const token = await jwt.sign(
+const token = jwt.sign(
   { name: 'halysson', email: 'halyssonfreitas@gmail.com' },
   privateKey,
   { expiresIn: '1h', algorithm: 'RS512' }
 );
 
-const decoded = await jwt.verify(token, privateKey);
+const decoded = jwt.verify(token, privateKey);
 
 console.log('token :::', token, '\n');
 console.log('json :::', JSON.stringify(decoded, null, 4));
